2334585f90e350dc62aed17b18ee6c5d9ac81f58
VidyaKore11 <126936892+VidyaKore11@users.noreply.github.com>
2023-09-16 17:35:56 +0530

    Create AddInt.py

---
+++ AddInt.py
@@ -0,0 +1,9 @@
+# Get user input for the two integers
+num1 = int(input("Enter the first integer: "))
+num2 = int(input("Enter the second integer: "))
+
+# Add the two integers
+sum = num1 + num2
+
+# Display the result
+print(f"The sum of {num1} and {num2} is {sum}")
